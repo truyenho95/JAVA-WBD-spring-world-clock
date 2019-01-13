9f4dc949e4de610abd27c74c6bcbdb0e9425da80
truyenho95 <truyenho.webdev@gmail.com>
2019-01-13 15:53:12 +0700

    refactoring

--- src/main/java/com/truyenho/controller/TimeController.java
+++ src/main/java/com/truyenho/controller/TimeController.java
@@ -15,14 +15,12 @@ public class TimeController {
 	
     @RequestMapping("/worldclock")
     public String getTimeByTimezone(ModelMap model, @RequestParam(name = "city", required = false, defaultValue = "Asia/Ho_Chi_Minh") String city) {
-        Date date = new Date(); // Get current time at local
-        TimeZone local = TimeZone.getDefault(); // Get timezone by the local
-        TimeZone locale = TimeZone.getTimeZone(city); // Get timezone by the specified city
+        Date date = new Date();
+        TimeZone timezoneByLocal = TimeZone.getDefault();
+        TimeZone timezoneBySpecifiedCity = TimeZone.getTimeZone(city);
+        long currentTimeInSpecifiedCity = date.getTime() + (timezoneBySpecifiedCity.getRawOffset() - timezoneByLocal.getRawOffset());
+        date.setTime(currentTimeInSpecifiedCity);
 
-        long locale_time = date.getTime() + (locale.getRawOffset() - local.getRawOffset()); // Calculate the current time in the specified city
-        date.setTime(locale_time); // Reset the date by locale_time
-        
-        // Set the data sent to the view
         model.addAttribute("city", city);
         model.addAttribute("date", date);
         return "worldclock";
